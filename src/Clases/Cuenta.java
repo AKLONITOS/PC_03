af744c51b17c08ec289e3126d480c7b0cc004453
UtrillaGuadalupeEdu <U23244502@utp.edu.pe>
2024-11-11 15:16:59 -0500

    Edu

--- src/Clases/Cuenta.java
+++ src/Clases/Cuenta.java
@@ -1,13 +1,91 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Clases;
 
-/**
- *
- * @author EDU
- */
+
 public class Cuenta {
+    String nombre;
+    String apellido;
+    int dni;
+    String usuario;
+    String clave;
+    String correo;
+    String estado;
+    String rol;
+    byte[] foto;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+
+    public byte[] getFoto() {
+        return foto;
+    }
+
+    public void setFoto(byte[] foto) {
+        this.foto = foto;
+    }
+    
+    
+    
     
 }
--- src/FuncionamientoTablas/TablaCuenta.java
+++ src/FuncionamientoTablas/TablaCuenta.java
@@ -1,13 +1,44 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package FuncionamientoTablas;
 
-/**
- *
- * @author EDU
- */
+import Clases.Cuenta;
+import Conexiones.Conexion;
+import java.io.FileInputStream;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+
 public class TablaCuenta {
+    Connection con;
+    Conexion cn=new Conexion();
+    PreparedStatement ps;
+    ResultSet rs;
+    FileInputStream fis;
     
+    public boolean AgregarCuenta(Cuenta CC) {
+        
+    String sql = "INSERT INTO cuentas (Nombre , Apellido , DNI , Usuario , Clave , Correo , Estado , Rol , Foto) VALUES (?,?,?,?,?,?,?,?,?)";
+    try {
+        FileInputStream archivofoto;
+        con = cn.getConnection();
+        ps = con.prepareStatement(sql);
+        ps.setString(1, CC.getNombre());
+        ps.setString(2, CC.getApellido());
+        ps.setInt(3, CC.getDNI());
+        ps.setString(4, CC.getUsuario());
+        ps.setString(5, CC.getClave());
+        ps.setString(6, CC.getCorreo());
+        ps.setString(7, CC.getEstado());
+        ps.setString(8, CC.getROL());
+        ps.setBytes(9, CC.getFoto());
+        ps.execute();
+        return true;
+    } catch (SQLException e) {
+        System.out.println(e.toString());
+        return false;
+    }
+    
+}
 }
