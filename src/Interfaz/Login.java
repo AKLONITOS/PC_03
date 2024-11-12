a331cc375d7388f77216d009a336deec8e0ca491
parejasmarceloroosevelt <U23244078@utp.edu.pe>
2024-11-11 22:34:45 -0500

    login1

--- src/Interfaz/Login.java
+++ src/Interfaz/Login.java
@@ -36,7 +36,7 @@ public class Login extends javax.swing.JFrame {
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGap(0, 586, Short.MAX_VALUE)
         );
 
         pack();
